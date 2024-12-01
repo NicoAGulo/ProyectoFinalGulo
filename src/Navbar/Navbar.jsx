f88d90e4caf1b0d70f682a8fa2dfb44557eba3df
NicoAGulo <nicogulo@gmail.com>
2024-12-01 19:52:42 -0300

    Integrando complemento Navbar

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,10 +1,15 @@
+import './navbar.css'
+
 function NavbarComponente(){
 
     console.log("Ingresaste a Droneosfera");
     
     return(
         <>
+        <div className="bloque-navbar">
         <h1>Droneosfera</h1>
+        <h2>Captura el espacio</h2>
+        </div>
         </>
     )
 }
