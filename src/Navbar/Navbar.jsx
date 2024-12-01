f68a566d65bfaf07f20465aedd11d3b0768a7699
NicoAGulo <nicogulo@gmail.com>
2024-12-01 20:29:42 -0300

    Primeras ediciones de navbar

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -7,6 +7,10 @@ function NavbarComponente(){
     return(
         <>
         <div className="bloque-navbar">
+            <div className="logo-navbar">
+                <img src="../src/assets/img/drone-2.png" alt="logo-drone"/>
+                </div>
+                
         <h1>Droneosfera</h1>
         <h2>Captura el espacio</h2>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import NavbarComponente from './Navbar/Navbar'
+import './index.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <header>
     <NavbarComponente />
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
   </StrictMode>,
 )
